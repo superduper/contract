c8973f3f76c2a3bcab61a90ae363be9f40671e41
Victor Miroshnikov <me@vmiroshnikov.com>
2014-04-11 20:24:06 +0200

    add error formatting

--- contract.go
+++ contract.go
@@ -4,8 +4,21 @@ import "fmt"
 
 var SuppressPanic bool = false
 
-func shoutAndPanic(descr string) {
-	err := fmt.Errorf(descr)
+func shoutAndPanic(descrf ...string) {
+	var err error
+	if len(descrf) == 0 {
+		err = fmt.Errorf("failed to meet the contract")
+	} else {
+		if len(descrf) == 1 {
+			err = fmt.Errorf(descrf[0])
+		} else {
+			fmtargs := make([]interface{}, len(descrf[1:]))
+			for i, v := range descrf[1:] {
+				fmtargs[i] = interface{}(v)
+			}
+			err = fmt.Errorf(descrf[0], fmtargs...)
+		}
+	}
 	if SuppressPanic {
 		fmt.Println(err)
 	} else {
@@ -13,49 +26,43 @@ func shoutAndPanic(descr string) {
 	}
 }
 
-func shoutAndPanicIf(orly bool, descr string) {
+func shoutAndPanicIf(orly bool, descrf ...string) {
 	if orly {
-		shoutAndPanic(descr)
+		shoutAndPanic(descrf...)
 	}
 }
 
-func Require(success bool, descr string) {
-	if !success {
-		descr := fmt.Sprintf("Input contract not met: %s", descr)
-		shoutAndPanic(descr)
+func Require(success bool, descrf ...string) {
+	shoutAndPanicIf(!success, descrf...)
+}
+
+func MustBeTrue(success bool, descrf ...string) {
+	shoutAndPanicIf(success == true, descrf...)
+}
+
+func MustNotBeNil(v interface{}, descrf ...string) {
+	shoutAndPanicIf(v == nil, descrf...)
+}
+
+func MustBeNil(v interface{}, descrf ...string) {
+	shoutAndPanicIf(v != nil, descrf...)
+}
+
+func RequireArg(v interface{}, descrf ...string) {
+	if len(descrf) == 0 {
+		descrf = []string{"nil passed instead of real value"}
 	}
+	MustNotBeNil(v, descrf...)
 }
 
-func MustBeTrue(success bool, descr string) {
-	shoutAndPanicIf(success == true, descr)
+func MustBeFalse(success bool, descrf ...string) {
+	shoutAndPanicIf(success == false, descrf...)
 }
 
-func MustNotBeNil(v interface{}, descr string) {
-	shoutAndPanicIf(v == nil, descr)
+func RequireNoError(err error, descrf ...string) {
+	shoutAndPanicIf(err != nil, descrf...)
 }
 
-func MustBeNil(v interface{}, descr string) {
-	shoutAndPanicIf(v != nil, descr)
-}
-
-func RequireArg(v interface{}) {
-	MustNotBeNil(v, "nil passed instead of real value")
-}
-
-func MustBeFalse(success bool, descr string) {
-	shoutAndPanicIf(success == false, descr)
-}
-
-func RequireNoError(err error, intent string) {
-	if err != nil {
-		descr := fmt.Sprintf("Required no error to do: %s, but got: %s", intent, err.Error())
-		shoutAndPanic(descr)
-	}
-}
-
-func Guarantee(success bool, descr string) {
-	if !success {
-		descr := fmt.Sprintf("Output contract not met: %s", descr)
-		shoutAndPanic(descr)
-	}
+func Guarantee(success bool, descrf ...string) {
+	shoutAndPanicIf(!success, descrf...)
 }
